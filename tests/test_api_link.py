be5d3eeabef68e4cdc06e11f5bff255e63a08415
Aleksey Leontiev <aleksey.leontiev@icloud.com>
2016-07-15 09:48:39 +0300

    test link

---
+++ tests/test_api_link.py
@@ -0,0 +1,26 @@
+import unittest
+
+from api.models import Thought, Link
+
+
+class ThoughtMethods(unittest.TestCase):
+    def setUp(self):
+        self.root = Thought()
+        self.child1 = Thought()
+        self.child2 = Thought()
+
+    def test_init(self):
+        l = Link(self.root, self.child1, "child")
+        self.assertEqual(l.source, self.root)
+        self.assertEqual(l.destination, self.child1)
+        self.assertEqual(l.kind, "child")
+
+    def test_eq(self):
+        l1 = Link(self.root, self.child1, "child")
+        l2 = Link(self.root, self.child1, "child")
+        self.assertEqual(l1, l2)
+
+    def test_not_eq(self):
+        l1 = Link(self.root, self.child1, "child")
+        l2 = Link(self.root, self.child2, "child")
+        self.assertNotEqual(l1, l2)
